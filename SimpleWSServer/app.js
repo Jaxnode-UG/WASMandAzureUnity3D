e5d0f443bc94a591b44df62eb2f30393817324e4
John Lyons <john.lyons@oceandisc.com>
2018-01-18 15:11:48 -0500

    Lyons Code commit

---
+++ SimpleWSServer/app.js
@@ -0,0 +1,29 @@
+const WebSocket = require('ws');
+ 
+const wss = new WebSocket.Server({ port: 8080 });
+
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+wss.on('connection', function connection(ws) {
+  
+  
+  ws.on('message', function incoming(data) {
+    console.log(data.toString('utf8'));
+    // Broadcast to everyone else.
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data.toString('utf8'));
+		    //console.log("Now here");
+      }
+    });
+  });
+});
+
+
---
+++ WebGL/Assets/Example/EchoTest.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EchoTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    WebSocket w;
+    string reply;
+    void Start()
+    {
+
+            w = new WebSocket(new Uri("ws://40.71.23.187:8080"));
+            StartCoroutine(w.Connect());
+
+        
+    }
+
+    void Update()
+    {
+
+            try
+            {
+                reply = w.RecvString();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error: " + e.ToString());
+               // break;
+            }
+
+            if (reply != null)
+            {
+                Debug.Log("Received: " + reply);
+
+            }
+            if (w.error != null)
+            {
+                Debug.LogError("Error: " + w.error.ToString());
+
+            }             
+             
+        
+    }
+
+    public void SendData()
+    {
+        w.SendString("Sending Data");
+    }
+
+
+    public void CloseConnection()
+    {
+        w.Close();
+    }
+}
